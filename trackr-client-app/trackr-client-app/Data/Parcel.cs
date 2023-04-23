43a8e036ea7552b2dc6cefe6700d554c4f0f6eb3
unknown <21522652@gm.uit.edu.vn>
2023-04-23 16:55:01 +0700

    Hiển thị đơn hàng từ data random tự tạo

---
+++ trackr-client-app/trackr-client-app/Data/Parcel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trackr_client_app.Data
+{
+    public class Parcel
+    {
+        public long parcelId { get; set; }
+        public string parcelName { get; set;}
+        public DateTime orderDate { get; set; }
+        public string parcelStatus { get; set;}
+        public DateTime estimateDate { get; set; }
+        public long cusId { get; set; }
+        public string cusName { get; set;}
+        public string cusPhone { get; set;}
+        public string cusAddress { get; set;}
+    }
+}
