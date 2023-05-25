a311ce19ba4f24dcfd22f293438f6aebb72a8c2c
unknown <21522652@gm.uit.edu.vn>
2023-05-26 00:02:56 +0700

    Thêm code DPI scaling

--- trackr-client-app/trackr-client-app/LoginScreen.cs
+++ trackr-client-app/trackr-client-app/LoginScreen.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace trackr_client_app
 {
-    public partial class Form1 : Form
+    public partial class LoginScreen : Form
     {
-        public Form1()
+        public LoginScreen()
         {
             InitializeComponent();
+        }
+
+        private void LoginScreen_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
