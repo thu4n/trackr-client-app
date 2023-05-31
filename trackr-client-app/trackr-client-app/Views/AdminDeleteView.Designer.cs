ed6592a6c1e26b13312dc04ebde4b3e259cf1033
unknown <21522652@gm.uit.edu.vn>
2023-05-31 14:34:03 +0700

    Dựng giao diện xóa object cho admin

---
+++ trackr-client-app/trackr-client-app/Views/AdminDeleteView.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace trackr_client_app.Views
+{
+    partial class AdminDeleteView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminDeleteView));
+            this.label1 = new System.Windows.Forms.Label();
+            this.yesBtn = new System.Windows.Forms.Button();
+            this.noBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(67, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(343, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bạn có chắc chắn muốn xóa không? \r\nHành động này không thể hoàn tác được.\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // yesBtn
+            // 
+            this.yesBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(111)))), ((int)(((byte)(111)))));
+            this.yesBtn.FlatAppearance.BorderSize = 0;
+            this.yesBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.yesBtn.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yesBtn.ForeColor = System.Drawing.Color.White;
+            this.yesBtn.Location = new System.Drawing.Point(79, 100);
+            this.yesBtn.Name = "yesBtn";
+            this.yesBtn.Size = new System.Drawing.Size(143, 33);
+            this.yesBtn.TabIndex = 5;
+            this.yesBtn.Text = "Tiếp tục xóa";
+            this.yesBtn.UseVisualStyleBackColor = false;
+            this.yesBtn.Click += new System.EventHandler(this.yesBtn_Click);
+            // 
+            // noBtn
+            // 
+            this.noBtn.BackColor = System.Drawing.Color.White;
+            this.noBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(111)))), ((int)(((byte)(111)))));
+            this.noBtn.FlatAppearance.BorderSize = 2;
+            this.noBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.noBtn.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(111)))), ((int)(((byte)(111)))));
+            this.noBtn.Location = new System.Drawing.Point(251, 100);
+            this.noBtn.Name = "noBtn";
+            this.noBtn.Size = new System.Drawing.Size(147, 33);
+            this.noBtn.TabIndex = 70;
+            this.noBtn.Text = "Hủy bỏ";
+            this.noBtn.UseVisualStyleBackColor = false;
+            this.noBtn.Click += new System.EventHandler(this.noBtn_Click);
+            // 
+            // AdminDeleteView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 183);
+            this.Controls.Add(this.noBtn);
+            this.Controls.Add(this.yesBtn);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AdminDeleteView";
+            this.Text = "Trackr - Delete";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button yesBtn;
+        private System.Windows.Forms.Button noBtn;
+    }
+}
---
+++ trackr-client-app/trackr-client-app/Views/AdminDeleteView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trackr_client_app.Views
+{
+    public partial class AdminDeleteView : Form
+    {
+        public AdminDeleteView()
+        {
+            InitializeComponent();
+        }
+
+        private void yesBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void noBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
