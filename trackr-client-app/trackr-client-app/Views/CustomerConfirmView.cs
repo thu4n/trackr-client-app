63d12d52f52738f1239cb557e281e80af2c5f6af
ngocdiemm <ngocdiem.le.2803@gmail.com>
2023-06-11 14:46:08 +0700

    Update code để nghỉ ngơi xíu

---
+++ trackr-client-app/trackr-client-app/Views/CustomerConfirmView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trackr_client_app.Views
+{
+    public partial class CustomerConfirmView : Form
+    {
+        public CustomerConfirmView()
+        {
+            InitializeComponent();
+        }
+
+        private void yesBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void noBtn_Click(object sender, EventArgs e)
+        {
+            CustomerParcelView customerParcelView = new CustomerParcelView();
+            customerParcelView.Show();
+        }
+    }
+}
